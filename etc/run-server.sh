c27d1f0158aa169d03a99b4a2904512091e719c9
s-aska <s.aska.org@gmail.com>
2011-11-09 09:05:35 +0900

    fix path

--- etc/run-server.sh
+++ etc/run-server.sh
@@ -2,4 +2,4 @@
 
 rvm_path=/home/chihaya-pm/.rvm;
 source /home/chihaya-pm/.rvm/scripts/rvm;
-rvm 1.9.2 exec ruby /home/chihaya-pm/screen/server.rb
+rvm 1.9.2 exec ruby /home/chihaya-pm/chihaya-pm-screen/server.rb
